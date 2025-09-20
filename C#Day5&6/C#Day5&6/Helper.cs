e0fd998b419a5fb3b4c802ce57b7a8262d7229a0
AyaEL-najaar <eaya5317@gmail.com>
2025-09-20 22:36:14 +0300

    Add files via upload

---
+++ C#Day5&6/C#Day5&6/Helper.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace C_Day5_6
+{
+    internal class Helper
+    {
+        // 1. Optimized Bubble Sort
+        public static void OptimizedBubbleSort<T>(T[] Array) where T : IComparable<T>
+        {
+            if (Array is not null)
+            {
+                for (int i = 0; i < Array.Length - 1; i++)
+                {
+                    bool swapped = false;
+                    for (int j = 0; j < Array.Length - i - 1; j++)
+                    {
+                        if (Array[j].CompareTo(Array[j + 1]) > 0)
+                        {
+                            T temp = Array[j];
+                            Array[j] = Array[j + 1];
+                            Array[j + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+                    if (!swapped) break;
+                }
+            }
+        }
+
+        // 2. Generic Range<T> Class
+        public class Range<T> where T : IComparable<T>
+        {
+            public T Min { get; }
+            public T Max { get; }
+
+            public Range(T min, T max)
+            {
+                if (min.CompareTo(max) > 0)
+                    throw new ArgumentException("Min must be <= Max");
+
+                Min = min;
+                Max = max;
+            }
+
+            public bool IsInRange(T value)
+            {
+                return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+            }
+
+            public dynamic Length()
+            {
+                dynamic min = Min;
+                dynamic max = Max;
+                return max - min;
+            }
+        }
+
+        // 3. Reverse ArrayList
+        public static void ReverseArrayList(ArrayList list)
+        {
+            if (list is not null)
+            {
+                int left = 0, right = list.Count - 1;
+                while (left < right)
+                {
+                    object temp = list[left];
+                    list[left] = list[right];
+                    list[right] = temp;
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        // 4. Get Even Numbers
+        public static List<int> GetEvenNumbers(List<int> numbers)
+        {
+            List<int> evens = new List<int>();
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                    evens.Add(num);
+            }
+            return evens;
+        }
+
+        // 5. FixedSizeList<T>
+        public class FixedSizeList<T>
+        {
+            private T[] items;
+            private int count;
+
+            public FixedSizeList(int capacity)
+            {
+                if (capacity <= 0)
+                    throw new ArgumentException("Capacity must be > 0");
+                items = new T[capacity];
+                count = 0;
+            }
+
+            public void Add(T item)
+            {
+                if (count >= items.Length)
+                    throw new InvalidOperationException("List is full!");
+                items[count++] = item;
+            }
+
+            public T Get(int index)
+            {
+                if (index < 0 || index >= count)
+                    throw new IndexOutOfRangeException("Invalid index!");
+                return items[index];
+            }
+        }
+
+        // 6. First Non-Repeated Character
+        public static int FirstNonRepeatedChar(string s)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (freq.ContainsKey(c))
+                    freq[c]++;
+                else
+                    freq[c] = 1;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (freq[s[i]] == 1)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ C#Day5&6/C#Day5&6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace C_Day5_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Assignment 1 Tests =====");
+
+            // 1. Optimized Bubble Sort
+            int[] nums = { 5, 1, 4, 2, 8 };
+            Helper.OptimizedBubbleSort(nums);
+            Console.WriteLine("Sorted Array: " + string.Join(",", nums));
+
+            // 2. Range<T>
+            var range = new Helper.Range<int>(10, 20);
+            Console.WriteLine($"Is 15 in range? {range.IsInRange(15)}");
+            Console.WriteLine($"Range Length: {range.Length()}");
+
+            // 3. Reverse ArrayList
+            ArrayList arrList = new ArrayList() { 1, 2, 3, 4, 5 };
+            Helper.ReverseArrayList(arrList);
+            Console.WriteLine("Reversed ArrayList: " + string.Join(",", arrList.ToArray()));
+
+            // 4. Get Even Numbers
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            var evens = Helper.GetEvenNumbers(numbers);
+            Console.WriteLine("Even Numbers: " + string.Join(",", evens));
+
+            // 5. FixedSizeList<T>
+            var fixedList = new Helper.FixedSizeList<string>(2);
+            fixedList.Add("Aya");
+            fixedList.Add("Ahmed");
+            Console.WriteLine($"FixedSizeList[0]: {fixedList.Get(0)}");
+            Console.WriteLine($"FixedSizeList[1]: {fixedList.Get(1)}");
+
+            // 6. First Non-Repeated Character
+            string s = "swiss";
+            int index = Helper.FirstNonRepeatedChar(s);
+            Console.WriteLine($"First non-repeated char index in '{s}': {index}");
+        }
+    }
+}
